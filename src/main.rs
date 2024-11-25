e652a8cfbc9cc357a404e01eecd791c04d3808b3
liou <liou666@126.com>
2024-11-25 18:58:52 +0800

    wip: mouse listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,135 @@
+use arboard::Clipboard;
+use mouse_position::mouse_position::Mouse;
+use rdev::{listen, Event, EventType};
+use std::{error::Error, sync::Mutex, time::Duration};
+
+pub static PREVIOUS_PRESS_TIME: Mutex<u128> = Mutex::new(0);
+pub static PREVIOUS_RELEASE_POSITION: Mutex<(i32, i32)> = Mutex::new((0, 0));
+
+struct POINT {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Start listening to mouse events...");
+
+    // Start event listener
+    if let Err(error) = listen(callback) {
+        println!("Failed to listen: {:?}", error);
+    }
+}
+
+fn callback(event: Event) {
+    match event.event_type {
+        EventType::ButtonPress(button) => {
+            if button == rdev::Button::Left {
+                println!("Left button pressed");
+                let current_press_time = std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis();
+                let position = Mouse::get_mouse_position();
+                match position {
+                    Mouse::Position { x, y } => {
+                        println!("x: {}, y: {}", x, y);
+                        if let Ok(mut previous_release_position) = PREVIOUS_RELEASE_POSITION.lock()
+                        {
+                            *previous_release_position = (x, y);
+                        } else {
+                            eprintln!("Unable to lock Mutex");
+                        }
+
+                        // if let Ok(mut previous_press_time) = PREVIOUS_PRESS_TIME.lock() {
+                        //     *previous_press_time = current_press_time;
+                        // } else {
+                        //     eprintln!("Unable to lock Mutex");
+                        // }
+                    }
+                    Mouse::Error => println!("Error getting mouse position"),
+                }
+            }
+        }
+        EventType::ButtonRelease(button) => {
+            if button == rdev::Button::Left {
+                println!("Left button released");
+
+                let mut end_point = POINT { x: 0, y: 0 };
+
+                let current_release_time = std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis();
+
+                let position = Mouse::get_mouse_position();
+                match position {
+                    Mouse::Position { x, y } => {
+                        println!("x: {}, y: {}", x, y);
+                        if let Ok(previous_release_position) = PREVIOUS_RELEASE_POSITION.lock() {
+                            let (prev_release_x, prev_release_y) = *previous_release_position;
+
+                            let mouse_distance = (((x - prev_release_x).pow(2)
+                                + (y - prev_release_y).pow(2))
+                                as f64)
+                                .sqrt();
+
+                            let is_double_click =
+                                current_release_time - *PREVIOUS_PRESS_TIME.lock().unwrap() < 500;
+
+                            println!(
+                                "Mouse move distance: {} - is_double_click: {}",
+                                mouse_distance, is_double_click
+                            );
+
+                            let is_text_select = mouse_distance > 15.0 || is_double_click;
+                            if is_text_select {
+                                println!("Possible text selection event");
+                                std::thread::spawn(move || {
+                                    let selected_text = get_text_by_clipboard().unwrap_or_default();
+                                    println!("Selected text: {}", selected_text);
+                                });
+                            }
+                            *PREVIOUS_PRESS_TIME.lock().unwrap() = current_release_time;
+                        } else {
+                            eprintln!("Unable to lock Mutex");
+                        }
+                    }
+                    Mouse::Error => println!("Error getting mouse position"),
+                }
+
+                // println!("position: {:#?}", position);
+                // let (x, y): (i32, i32) = get_mouse_location().unwrap();
+                // let (prev_release_x, prev_release_y) = { *PREVIOUS_RELEASE_POSITION.lock() };
+                // {
+                //     *PREVIOUS_RELEASE_POSITION.lock() = (x, y);
+                // }
+
+                // if let Ok(previous_press_time) = PREVIOUS_PRESS_TIME.lock() {
+                //     let press_duration = current_release_time - *previous_press_time;
+                //     // You can add duration judgment logic here
+                //     println!("Press duration: {}ms", press_duration);
+
+                //     // Logic to determine if it is a text selection event
+                //     if press_duration > 50 && press_duration < 1000 {
+                //         println!("Possible text selection event");
+                //     }
+                // } else {
+                //     eprintln!("Unable to read Mutex");
+                // }
+            }
+        }
+        EventType::MouseMove { x, y } => {
+            // If you need to handle mouse move events, you can add code here
+            // println!("Mouse moved to: ({}, {})", x, y);
+        }
+        _ => {}
+    }
+}
+
+fn get_text_by_clipboard() -> Result<String, Box<dyn Error>> {
+    let old_clipboard = Clipboard::new()?.get_text();
+    match old_clipboard {
+        Ok(text) => Ok(text),
+        Err(error) => Err(Box::new(error)),
+    }
 }
---
+++ src/utils.rs
@@ -0,0 +1,9 @@
+use mouce::{Mouse, MouseActions};
+
+// pub fn get_mouse_location() -> Result<(i32, i32), String> {
+//     let position = Mouse::get_position();
+//     match position {
+//         Mouse::Position { x, y } => Ok((x, y)),
+//         Mouse::Error => Err("Error getting mouse position".to_string()),
+//     }
+// }
